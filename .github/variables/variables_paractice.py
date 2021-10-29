49ee427161f6d241df7d2afc928d1387f46701bd
Jahaun G <s11088077@students.duvalschools.org>
2021-10-29 14:39:57 -0400

    version 0.1
    Added integers, floating point, and string

--- .github/variables/variables_paractice.py
+++ .github/variables/variables_paractice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Jahaun Gilmore, 10/29/21 2:25pm, version 0.0
+# Variables Practice, Jahaun Gilmore, 10/29/21 2:37pm, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 7 # INTERGERS, any +/- WHOLE number and 0.
+forty_time = 4.16 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "Curse you! You will pay for this one day!"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
