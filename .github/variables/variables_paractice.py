255c224bcc77d4b5aab4114e74d6f3aaf1a97b7c
Jahaun G <s11088077@students.duvalschools.org>
2021-10-29 14:46:18 -0400

    version 0.2
    Added print(lives_remaining), print(forty_time), and print(death_cry)

--- .github/variables/variables_paractice.py
+++ .github/variables/variables_paractice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Jahaun Gilmore, 10/29/21 2:37pm, version 0.1
+# Variables Practice, Jahaun Gilmore, 10/29/21 2:42pm, version 0.2
 
 # Three Common Types of Data
 
@@ -6,3 +6,7 @@ lives_remaining = 7 # INTERGERS, any +/- WHOLE number and 0.
 forty_time = 4.16 # FLOATING POINT, any +/- number with a decimal.
 death_cry = "Curse you! You will pay for this one day!"
 # Line 7 is a STRING -- collection of letters/numbers/other chars.
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
